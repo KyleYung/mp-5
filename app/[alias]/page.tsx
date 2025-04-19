636a84ef781947ccf6c9ac73611164ea235076f0
KyleYung <kylehyung@gmail.com>
2025-04-18 21:59:18 -0400

    edited alias page

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,7 +1,7 @@
 import { redirect, notFound } from "next/navigation";
 import getCollection, { URL_COLLECTION } from "@/db";
 
-export default async function AliasPage({params} : {params: {alias: string}}) {
+export default async function AliasPage({params} : {params: Promise<{alias: string}>}) {
     const {alias} = await params;
 
     const collection = await getCollection(URL_COLLECTION);
